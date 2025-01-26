d3e6b6adfd7e85cf878ea30a2b167389794b204b
ag0532211 <f43d@baldheadcat.com>
2025-01-26 11:52:58 +0800

    update custom.js -update header.html to override default theme

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,10 +1,14 @@
 // Disable theme toggle functionality
 document.addEventListener('DOMContentLoaded', function () {
+    console.log('Custom JavaScript is loaded!');
     const themeToggle = document.querySelector('.theme-toggle');
     if (themeToggle) {
+      console.log('Theme toggle button found and removed');
       themeToggle.remove(); // Remove the toggle button
+    } else {
+      console.log('Theme toggle button not found');
     }
     document.documentElement.setAttribute('data-theme', 'light'); // Force light mode
-  });
+  });  
   
   
